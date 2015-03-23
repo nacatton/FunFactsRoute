a596a2ba41a120026b304543b74f792ce72879d9
nacatton <nacatton@Nicholass-MBP.lan>
2015-03-23 12:00:09 -0300

    Deleted facts

--- app/src/main/java/com/nacatton/funfacts/FactBook.java
+++ app/src/main/java/com/nacatton/funfacts/FactBook.java
@@ -11,15 +11,6 @@ public class FactBook {
     public String[] mFacts = {
             "Juli and Nick will make an amazing app.",
             "We will make something functional.",
-            "Olympic gold medals are actually made mostly of silver.",
-            "You are born with 300 bones; by the time you are an adult you will have 206.",
-            "It takes about 8 minutes for light from the Sun to reach Earth.",
-            "Some bamboo plants can grow almost a meter in just one day.",
-            "The state of Florida is bigger than England.",
-            "Some penguins can leap 2-3 meters out of the water.",
-            "On average, it takes 66 days to form a new habit.",
-            "Mammoths still walked the earth when the Great Pyramid was being built.",
-            "Treehouse is not actually in a tree",
             "Life is difficult"};
 
     // Method (abilities: things the objects can do)
